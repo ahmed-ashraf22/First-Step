6d30d6270d9526916e22142f7f3ffbd360874a32
Ahmed Ashraf <engahmedashraf2210@gmail.com>
2018-02-07 00:32:41 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+int x;
+int y;
+int p;
+} rest;
+int main()
+{
+    int n,x,y,p;
+    scanf("%d",&n);
+
+    rest arr [n];
+
+
+
+
+
+
+    return 0;
+}
